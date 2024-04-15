7063f5395b0c3e40137c053a71cbddbd09c89271
Serhii <kch_i@i.ua>
2024-04-15 20:52:26 +0300

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,7 +10,6 @@ import {
 } from './redux/store'; 
 import './styles/index.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
